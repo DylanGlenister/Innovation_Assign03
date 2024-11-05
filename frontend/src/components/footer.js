51ba65d3ca5798465f2d81d220c4d0a4181dd90e
YIPPEEautist <104553948@student.swin.edu.au>
2024-11-05 22:39:07 +1100

    fixes

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+function Footer() {
+    return (
+      <div>
+        <footer>
+            <p>Copyright Group 69 @ Swinburne 2024</p>
+        </footer>
+      </div>
+    );
+}
+
+export default Navbar;
